e69bc24d8122e980f59dcd99b037d1efcbd658ae
Anand Thakker <anandthakker@users.noreply.github.com>
2016-02-15 12:52:18 -0500

    Use roundTowardZero for better code clarity

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ module.exports = function (ms) {
 	if (typeof ms !== 'number') {
 		throw new TypeError('Expected a number');
 	}
+	
+	var roundTowardZero = ms > 0 ? Math.floor : Math.ceil;
 
 	return {
-		days: (ms / 86400000) | 0,
-		hours: (ms / 3600000 % 24) | 0,
-		minutes: (ms / 60000 % 60) | 0,
-		seconds: (ms / 1000 % 60) | 0,
-		milliseconds: (ms % 1000) | 0
+		days: roundTowardZero(ms / 86400000),
+		hours: roundTowardZero(ms / 3600000) % 24,
+		minutes: roundTowardZero(ms / 60000) % 60,
+		seconds: roundTowardZero(ms / 1000) % 60,
+		milliseconds: roundTowardZero(ms) % 1000
 	};
 };
