3178299381030422e191d1656e427848e13305a3
Mees van Dijk <mees@itisme.es>
2018-07-03 16:30:33 +0200

    Add µs and ns to output

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ module.exports = function (ms) {
 		hours: roundTowardZero(ms / 3600000) % 24,
 		minutes: roundTowardZero(ms / 60000) % 60,
 		seconds: roundTowardZero(ms / 1000) % 60,
-		milliseconds: roundTowardZero(ms) % 1000
+		milliseconds: roundTowardZero(ms) % 1000,
+		microseconds: roundTowardZero(ms * 1000) % 1000,
+		nanoseconds: roundTowardZero(ms* 1e6) % 1000
 	};
 };
