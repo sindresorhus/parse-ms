fcaf6eea455f2727d8eff79c9ebc9d0731efcf2b
[1000i100] Millicent Billette <git@1000i100.fr>
2022-12-05 10:01:03 +0100

    v3.1.0 : now handle short output format

--- index.mjs
+++ index.mjs
@@ -1,18 +1,19 @@
 const weekByMonth = 365.25 / 7 / 12;
 
 const factors = [
-	{unit: 'nanoseconds', modulo: 1000, factor: 1e-6},
-	{unit: 'microseconds', modulo: 1000},
-	{unit: 'milliseconds', modulo: 1000},
-	{unit: 'seconds', modulo: 60},
-	{unit: 'minutes', modulo: 60},
-	{unit: 'hours', modulo: 24},
-	{unit: 'days', modulo: 7},
-	{unit: 'weeks', modulo: weekByMonth},
-	{unit: 'months', modulo: 12},
-	{unit: 'years', modulo: 100},
-	{unit: 'centuries', modulo: 10},
-	{unit: 'millennia', modulo: 999_999_999_999_999},
+	{shortUnit: 'ps', unit: 'picoseconds', modulo: 1000, factor: 1e-9},
+	{shortUnit: 'ns', unit: 'nanoseconds', modulo: 1000, factor: 1e-6},
+	{shortUnit: 'µs', unit: 'microseconds', modulo: 1000},
+	{shortUnit: 'ms', unit: 'milliseconds', modulo: 1000},
+	{shortUnit: 's', unit: 'seconds', modulo: 60},
+	{shortUnit: 'm', unit: 'minutes', modulo: 60},
+	{shortUnit: 'h', unit: 'hours', modulo: 24},
+	{shortUnit: 'D', unit: 'days', modulo: 7},
+	{shortUnit: 'W', unit: 'weeks', modulo: weekByMonth},
+	{shortUnit: 'M', unit: 'months', modulo: 12},
+	{shortUnit: 'Y', unit: 'years', modulo: 100},
+	{shortUnit: 'c', unit: 'centuries', modulo: 10},
+	{shortUnit: 'ky', unit: 'millennia', modulo: 999_999_999_999_999},
 ];
 for (let i = 1; i < factors.length; i++) {
 	factors[i].factor = factors[i - 1].factor * factors[i - 1].modulo;
@@ -30,24 +31,39 @@ if (lastFactor.factor >= 1) {
 
 factors.reverse();
 
-const factorsUnit = factors.map(f => f.unit);
+const toLongUnit = {};
+for (const f of factors) {
+	toLongUnit[f.unit] = f.unit;
+	toLongUnit[f.shortUnit] = f.unit;
+}
 
-export default function parseMilliseconds(milliseconds, upToUnit = 'days') {
+const toShortUnit = {};
+for (const f of factors) {
+	toShortUnit[f.unit] = f.shortUnit;
+	toShortUnit[f.shortUnit] = f.shortUnit;
+}
+
+export default function parseMilliseconds(milliseconds, upToUnit = 'days', downToUnit = 'nanoseconds', outputFormat = 'long') {
 	if (typeof milliseconds !== 'number') {
 		throw new TypeError(`Expected a number, received : ${milliseconds}`);
 	}
 
-	if (!factorsUnit.includes(upToUnit)) {
-		throw new TypeError(`Unknow unit : ${upToUnit} (known : ${factorsUnit.join(', ')}`);
+	if (toLongUnit[upToUnit]) {
+		upToUnit = toLongUnit[upToUnit];
+	} else {
+		throw new TypeError(`Unknown unit : ${upToUnit} (known : ${toLongUnit.join(', ')}`);
+	}
+
+	if (toLongUnit[downToUnit]) {
+		downToUnit = toLongUnit[downToUnit];
+	} else {
+		throw new TypeError(`Unknown unit : ${downToUnit} (known : ${toLongUnit.join(', ')}`);
 	}
 
 	let intPower = 0;
 	while (!Number.isInteger(milliseconds)) {
 		milliseconds *= 10;
 		intPower++;
-		if (intPower >= 6) {
-			milliseconds = Math.round(milliseconds);
-		}
 	}
 
 	const result = {};
@@ -61,11 +77,41 @@ export default function parseMilliseconds(milliseconds, upToUnit = 'days') {
 			continue;
 		}
 
+		if (u.unit === downToUnit) {
+			result[u.unit] = Math.round(milliseconds / u.factor / (10 ** intPower));
+			return choosNameFormat(result, outputFormat);
+		}
+
 		result[u.unit] = Math.trunc(milliseconds / u.factor / (10 ** intPower));
 		if (result[u.unit]) {
 			milliseconds -= result[u.unit] * u.factor * (10 ** intPower);
 		}
 	}
 
-	return result;
+	return choosNameFormat(result, outputFormat);
+}
+
+function choosNameFormat(result, format = 'long') {
+	if (format === 'long') {
+		return result;
+	}
+
+	if (format === 'short') {
+		return convertToShort(result);
+	}
+
+	if (format === 'both') {
+		return {...convertToShort(result), ...result};
+	}
+
+	throw new TypeError(`output format "${format}" unknown. Choose between "short", "long" or "both".`);
+}
+
+function convertToShort(result) {
+	const shortFormat = {};
+	for (const k of Object.keys(result)) {
+		shortFormat[toShortUnit[k]] = result[k];
+	}
+
+	return shortFormat;
 }
--- test.mjs
+++ test.mjs
@@ -10,13 +10,13 @@ const zeroReference = {
 	microseconds: 0,
 	nanoseconds: 0,
 };
-const zeroFullReference = {...{
+const zeroFullReference = {
 	millennia: 0,
 	centuries: 0,
 	years: 0,
 	months: 0,
 	weeks: 0,
-},...zeroReference};
+	...zeroReference};
 
 function testParseMilliseconds(input, expectedOutput) {
 	test(`parseMilliseconds(${input}) -> ${JSON.stringify(expectedOutput)}`, t => {
@@ -33,7 +33,7 @@ testParseMilliseconds(1000 * 60 * 60 * 12, {hours: 12});
 testParseMilliseconds(1000 * 60 * 60 * 40, {days: 1, hours: 16});
 testParseMilliseconds(0.000_543, {nanoseconds: 543});
 testParseMilliseconds(1000 * 60 * 60 * 999, {days: 41,	hours: 15});
-testParseMilliseconds(500 + 0.345_678, {milliseconds: 500, microseconds: 345, nanoseconds: 678});
+testParseMilliseconds(0.123_456_789, {microseconds: 123, nanoseconds: 457});
 testParseMilliseconds((1000 * 60) + 500 + 0.345_678, {
 	minutes: 1,
 	seconds: 0,
@@ -50,9 +50,23 @@ test('split days in bigger units if asked', t => {
 		minutes: 30,
 	});
 });
+test('up and down unit can be given in short format and so for output', t => {
+	t.deepEqual(parseMilliseconds(0.123_456_789, 's', 's', 'short'), {s: 0});
+	t.deepEqual(parseMilliseconds(999, 's', 's', 'both'), {s: 1, seconds: 1});
+});
+test('round smaller units if asked', t => {
+	t.deepEqual(parseMilliseconds(0.123_456_789, 's', 's'), {seconds: 0});
+	t.deepEqual(parseMilliseconds(999, 's', 's'), {seconds: 1});
+});
+test('go deeper in small units if asked', t => {
+	t.deepEqual(parseMilliseconds(0.123_456_789, 'millennia', 'picoseconds'),
+		{...zeroFullReference, microseconds: 123, nanoseconds: 456, picoseconds: 789});
+});
 test('throw when bad input', t => {
 	t.throws(() => parseMilliseconds('a string'));
 	t.throws(() => parseMilliseconds(42, 'unknown unit'));
+	t.throws(() => parseMilliseconds(42, 's', ''));
+	t.throws(() => parseMilliseconds(42, 's', 's', 'nop'));
 });
 
 test('handle negative millisecond values', t => {
