b2221004589ecbb6297014d4138c9d56f58ac7df
fisker Cheung <lionkay@gmail.com>
2024-01-07 12:07:58 +0800

    Fix `NaN` microseconds and nanoseconds and require Node.js 18 (#20)

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
+const toZeroIfInfinity = value => Number.isFinite(value) ? value : 0;
+
 export default function parseMilliseconds(milliseconds) {
 	if (typeof milliseconds !== 'number') {
 		throw new TypeError('Expected a number');
 	}
 
 	return {
-		days: Math.trunc(milliseconds / 86400000),
-		hours: Math.trunc(milliseconds / 3600000) % 24,
-		minutes: Math.trunc(milliseconds / 60000) % 60,
-		seconds: Math.trunc(milliseconds / 1000) % 60,
-		milliseconds: Math.trunc(milliseconds) % 1000,
-		microseconds: Math.trunc(milliseconds * 1000) % 1000,
-		nanoseconds: Math.trunc(milliseconds * 1e6) % 1000
+		days: Math.trunc(milliseconds / 86_400_000),
+		hours: Math.trunc(milliseconds / 3_600_000 % 24),
+		minutes: Math.trunc(milliseconds / 60_000 % 60),
+		seconds: Math.trunc(milliseconds / 1000 % 60),
+		milliseconds: Math.trunc(milliseconds % 1000),
+		microseconds: Math.trunc(toZeroIfInfinity(milliseconds * 1000) % 1000),
+		nanoseconds: Math.trunc(toZeroIfInfinity(milliseconds * 1e6) % 1000),
 	};
 }
